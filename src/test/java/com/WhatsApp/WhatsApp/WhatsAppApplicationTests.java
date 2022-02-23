8c1be47036d4cf5ba26684d5ec99b4ca21a3a04c
rina9841 <pandyrina@gmail.com>
2022-02-23 15:25:16 -0500

    adding maven project along with pom.xml and java files

---
+++ src/test/java/com/WhatsApp/WhatsApp/WhatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WhatsApp.WhatsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
